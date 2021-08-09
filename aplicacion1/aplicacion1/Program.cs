8087e53958a2acda2a912a8a938039d73f90cf95
Fátima Lourdes Santos Guzmán <201800240@colsanjose.edu.gt>
2021-08-09 12:43:12 -0600

    cambio1

---
+++ aplicacion1/aplicacion1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace aplicacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string archivo;
+            char op = 's';
+            int OP = 1;
+            while(op=='s')
+            {
+                Console.Clear();
+                Console.WriteLine("Que decea hacer: \n 1.Nombre del archivo \n 2.Salir");
+                o = int.Parse(Console.ReadLine());
+
+                if(OP==1)
+                {
+                    Console.Clear();
+                    StreamWrite ar;
+                    archivo = Console.ReadLine();
+                    Console.WriteLine("Ingrese nombre del archivo: ");
+                    ar = File.AppendText(Console.ReadLine()+".txt");
+                    ar = File.CreateText();
+                    ar.Close();
+                }
+                if (OP == 2)
+                {
+                    Console.WriteLine("Usted a salido");
+
+                }
+
+                Console.Clear();
+                Console.WriteLine("Desea Continuar ['s/n']");
+                op = char.Parse(Console.ReadLine());
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

1a1a6b7bd9aee2d00202c073cdb0d03e36f280bf
Fátima Lourdes Santos Guzmán <201800240@colsanjose.edu.gt>
2021-08-09 12:52:41 -0600

    cambio4

--- aplicacion1/aplicacion1/Program.cs
+++ aplicacion1/aplicacion1/Program.cs
@@ -18,16 +18,15 @@ namespace aplicacion1
             {
                 Console.Clear();
                 Console.WriteLine("Que decea hacer: \n 1.Nombre del archivo \n 2.Salir");
-                o = int.Parse(Console.ReadLine());
+                OP = int.Parse(Console.ReadLine());
 
                 if(OP==1)
                 {
                     Console.Clear();
-                    StreamWrite ar;
+                    StreamWriter ar;
                     archivo = Console.ReadLine();
                     Console.WriteLine("Ingrese nombre del archivo: ");
                     ar = File.AppendText(Console.ReadLine()+".txt");
-                    ar = File.CreateText(@"C: \Users\Raul Santos Ramirez\Desktop\Trabajos de Lourdes\Programacion\zCuarta Unidad");
                     ar.Close();
                 }
                 if (OP == 2)
